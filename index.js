8488a3ffc541fd358fdb27cf86fc0f41452ecfb7
Doni Rahma Tiana <donirahmatiana@gmail.com>
2020-11-05 14:12:20 +0700

    remote jalan lah please

--- index.js
+++ index.js
@@ -37,8 +37,6 @@ app.use(function (err, req, res, next) {
 });
 
 const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log('App listening at http://%s:%s', host, port);
+app.listen(PORT, () => {
+  console.log('Server is running on port' + PORT);
 });
